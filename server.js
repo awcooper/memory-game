19860ad0b33396da95f528f5fdc5491d81c7f188
awcooper <wesner.cooper@gmail.com>
2018-12-20 22:58:32 -0500

    add basic game settings

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 
-let allWords = '';
+let words = [];
 
 fs.readFile('public/common_words.txt', "utf8", function(err, data) {
-    allWords = data.split('\n');
+    words = data.split('\n');
 });
 
 const app = express();
@@ -15,9 +15,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/words', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET');
-    res.send({ express: allWords });
+    res.send({ words });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- src/App.js
+++ src/App.js
@@ -5,27 +5,85 @@ import { getAllWords } from './api-calls'
 
 class App extends Component {
 
-    constructor(props){
-        super(props);
-        this.state = {
-            words: []
-        }
+  constructor(props){
+      super(props);
+      this.state = {
+          words: [],
+          numberOfWords: 10,
+          time: 60, //Time in seconds
+          gamePhase: 'settings',
+          currentGameWords: [],
+      }
+   };
+
+   componentDidMount(){
+       getAllWords().then(( { data: { words } } ) => this.setState({ words }))
+   }
+
+    handleChange(event) {
+        event.preventDefault();
+        const name = event.target.name;
+        const value = event.target.value;
+        this.setState({[`${name}`]: value})
     }
 
-  componentDidMount(){
-      getAllWords().then(( { data: { express: words } } ) => this.setState({ words }))
-  }
+   getRandomElements(n, l){
+       //Get n random elements from list l
+       const shuffled = l.sort(() => .5 - Math.random());
+       return shuffled.slice(0,n);
+   }
 
-  render() {
-    const { words } = this.state;
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome To My Little Word Game</h1>
+   buildGame = () => {
+       const { words, numberOfWords, time } = this.state;
+       this.setState({
+           timeRemaining: time,
+           gamePhase: 'play',
+           currentGameWords: this.getRandomElements(numberOfWords,words)
+       });
+   };
+
+   render() {
+     const { time, numberOfWords, gamePhase, currentGameWords } = this.state;
+     return (
+       <div className="App">
+         <header className="App-header">
+           <img src={logo} className="App-logo" alt="logo" />
+           <h1 className="App-title">Welcome To My Little Word Game</h1>
         </header>
-          <div>
-              { words.map( (word) => ( <h3> { word } </h3>) ) }
+           <div>
+               { gamePhase == 'settings' && ( <div id="settings">
+                   <input
+                       type="number"
+                       name="time"
+                       min="1"
+                       max="300"
+                       value="10"
+                       value={time}
+                       onChange={this.handleChange.bind(this)}
+                   />
+                   <br/>
+                   <input
+                       type="number"
+                       name="numberOfWords"
+                       min="1"
+                       max="300"
+                       value={numberOfWords}
+                       onChange={this.handleChange.bind(this)}
+                    />
+                   <br/>
+                   <button name="start" onClick={this.buildGame}>START</button>
+                   <h3> { time } </h3>
+                   <h3> { numberOfWords } </h3>
+               </div> )}
+               { gamePhase === 'play'  && (
+                   <div id="gameBox">
+                    <input type="text" name="input"/>
+                     <ol>
+                         { currentGameWords.map( (word, i) => <li key={i}>{ word }</li>) }
+                     </ol>
+                  </div>
+                  )
+              }
           </div>
       </div>
     );
